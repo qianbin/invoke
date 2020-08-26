ab911883e13dec9c48289e6bba0903c643666182
qianbin <cola.tin.com@gmail.com>
2020-08-26 17:58:10 +0800

    sign via spa

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <input type="button" value="Sign Auto" />
-    <input type="button" value="Sign via SPA" />
+    <input type="button" value="Sign via SPA" @click="signViaSPA" />
   </div>
 </template>
 
@@ -12,6 +12,57 @@ import { randomBytes } from "crypto";
 
 export default Vue.extend({
   name: "App",
+  methods: {
+    signViaSPA() {
+      this.uploadRequest().then((id) => {
+        window.open(`http://127.0.0.1:8080/#sign?rid=${id}`);
+      });
+    },
+    uploadRequest() {
+      const req = this.buildRequest();
+      const body = JSON.stringify(req);
+      const reqId = blake2b256(body).toString("hex");
+      return fetch("https://tos.vecha.in:5678/" + reqId, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body,
+      })
+        .then(() => {
+          return reqId;
+        })
+        .catch((err) => {
+          alert("error:" + err);
+        });
+    },
+    buildRequest() {
+      return {
+        secret: randomBytes(16).toString("hex"),
+        type: "tx",
+        gid:
+          "0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127",
+        body: {
+          message: [
+            {
+              to: "0x9840AcBcd7417ceEe8117dA585a3C3F6642c8a52",
+              value: "1000000000000000000",
+              comment: "this is clause comment",
+            },
+          ],
+          options: {
+            comment: "this is tx comment",
+            /*signer?: string
+                            gas?: number
+                            dependsOn?: string
+                            link?: string
+                            comment?: string
+                            delegate?: string*/
+          },
+        },
+      };
+    },
+  },
 });
 </script>
 
